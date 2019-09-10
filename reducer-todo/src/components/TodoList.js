c341c31ad694934ca1b55d25b540f29b8cef4922
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-10 13:38:09 -0600

    data rendering from reducer

--- reducer-todo/src/components/TodoList.js
+++ reducer-todo/src/components/TodoList.js
@@ -4,10 +4,17 @@ import { initialState, todoReducer } from '../reducers/todoReducer';
 
 const TodoList = () => {
     const [state, dispatch] = useReducer(todoReducer, initialState)
+    console.log(state);
 
     return (
         <div>
             <h1>List of Todos:</h1>
+            {state.map(item =>(
+                <ul> 
+                <li>Todo: {item.item} </li>
+                <li>Completed: {item.completed}</li>
+                </ul>
+            ))}
         </div>
     )
 }
--- reducer-todo/src/reducers/todoReducer.js
+++ reducer-todo/src/reducers/todoReducer.js
@@ -1,8 +1,18 @@
-export const initialState = {
+export const initialState = [{
     item: 'Take out trash',
     completed: false,
     id: 6547658
-}
+},
+{
+    item: 'Clean room',
+    completed: false,
+    id: 985293
+},
+{
+    item: 'Do homework',
+    completed: false,
+    id: 2345363
+}]
 
 export const todoReducer = (state, action) => {
     //actions: ADD_TODO, TOGGLE_TODO
