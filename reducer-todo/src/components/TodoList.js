74763e7893c5b07e9044d1c033327f958bca28ae
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-10 14:32:31 -0600

    ADD_TODO dipatch added to button

--- reducer-todo/src/components/TodoList.js
+++ reducer-todo/src/components/TodoList.js
@@ -2,21 +2,26 @@ import React, { useReducer } from 'react';
 
 import { initialState, todoReducer } from '../reducers/todoReducer';
 
+
+
 const TodoList = () => {
     const [state, dispatch] = useReducer(todoReducer, initialState)
     console.log(state);
+
 
     return (
         <div>
             <h1>List of Todos:</h1>
             {state.map(item =>(
-                <ul> 
+                <ol> 
                 <li>Todo: {item.item} </li>
-                <li>Completed: {item.completed}</li>
-                </ul>
+                <li className="idList">ID: {item.id}</li>
+                <li className="completedList">Completed: {item.completed}</li>
+                </ol>
             ))}
         <div className = "formContainer">
-            
+            <input className = "itemField"/>
+            <button onClick ={() => dispatch({ type: 'ADD_TODO' })}>Add Item to List</button>
         </div>
 
         </div>
