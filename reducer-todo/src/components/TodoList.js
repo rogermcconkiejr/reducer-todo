0d1cb228b61472badd1c77668c1c0cbc4b2a3b95
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-10 11:58:53 -0600

    todoList imported to app

--- reducer-todo/src/components/TodoList.js
+++ reducer-todo/src/components/TodoList.js
@@ -1,3 +1,14 @@
 import React, { useReducer } from 'react';
 
 import { initialState, todoReducer } from '../reducers/todoReducer';
+
+const TodoList = () => {
+
+    return (
+        <div>
+            <h1>List of Todos:</h1>
+        </div>
+    )
+}
+
+export default TodoList;
