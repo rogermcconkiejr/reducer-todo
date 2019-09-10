eaa4562e9d2f64bc7187ba86049d369c3bbc72cb
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-10 11:51:19 -0600

    initial state set and todoReducer started

---
+++ reducer-todo/src/reducers/todoReducer.js
@@ -0,0 +1,10 @@
+export const initialState = {
+    item: 'Take out trash',
+    completed: false,
+    id: 6547658
+}
+
+export const todoReducer = (state, action) => {
+    //actions: ADD_TODO, TOGGLE_TODO
+    return state;
+}
